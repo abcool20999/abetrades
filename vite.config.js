45171bced3ac44ec0806713327c654b513d8b150
abcool20999 <abcool20999@gmail.com>
2024-09-13 16:21:06 +0100

    latest

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
+    base: "/",
     plugins: [react()],
     root: path.resolve(__dirname, 'src'), 
     resolve: {
