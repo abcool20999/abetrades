fbbe6133ed7ee37e079a4b8dcafdd6d310c6285e
abcool20999 <abcool20999@gmail.com>
2024-09-14 18:55:19 +0100

    changes

--- src/components/Layout/Footer/FooterSocialMedia.jsx
+++ src/components/Layout/Footer/FooterSocialMedia.jsx
@@ -1,7 +1,7 @@
 // components/Layout/Footer.js
 
 import React from 'react';
-
+import appleUrl from '../../../assets/socialmedia/applestore.png'
 const FooterSocialMedia = () => {
   return (
     <div>
@@ -9,17 +9,17 @@ const FooterSocialMedia = () => {
         Find us on App Stores
       </h4>
       <div className="text-white">
-        <img style={{width: '120px', height: '40px'}} src='/assets/socialmedia/applestore.png'></img>
-        <img style={{width: '120px', height: '40px'}} src='/assets/socialmedia/googleplay.png'></img>
+        <img style={{width: '120px', height: '40px'}} src={appleUrl}></img>
+        <img style={{width: '120px', height: '40px'}} src='..\..\..\assets\socialmedia\googleplay.png'></img>
       </div>
       <div className="text-white">
         Join Us
       </div>
       <div className="text-white row">
-        <img className='col-2' style={{width: '20px', height: '20px'}} src='/assets/socialmedia/youtube.png'></img>
-        <img className='col-2' style={{width: '10px', height: '10px'}} src='/assets/socialmedia/facebook.png'></img>
-        <img className='col-2' style={{width: '10px', height: '10px'}} src='/assets/socialmedia/x.png'></img>
-        <img className='col-2' style={{width: '10px', height: '10px'}} src='/assets/socialmedia/instagram.png'></img>
+        <img className='col-2' style={{width: '20px', height: '20px'}} src='..\..\..\assets\socialmedia\youtube.png'></img>
+        <img className='col-2' style={{width: '10px', height: '10px'}} src='..\..\..\assets\socialmedia\facebook.png'></img>
+        <img className='col-2' style={{width: '10px', height: '10px'}} src='..\..\..\assets\socialmedia\x.png'></img>
+        <img className='col-2' style={{width: '10px', height: '10px'}} src='..\..\..\assets\socialmedia\instagram.png'></img>
       </div>
     </div>
   );
--- src/components/Pages/HomePage/Heading.jsx
+++ src/components/Pages/HomePage/Heading.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Heading.css';
+import forexUrl from '../../../assets/forex.jpeg'
 
 
 const Heading = () => {
     return (
-      <div id='homepage_heading' className='pb-5 bg-image pt-5 align-items-center' style={{backgroundImage: "url('/assets/forex.jpeg')", backgroundSize: 'cover', height: "500px"}}>
+      <div id='homepage_heading' className='pb-5 bg-image pt-5 align-items-center' style={{backgroundImage: `url(${forexUrl})`, backgroundSize: 'cover', height: "500px"}}>
         <h1 className='text-white p-2 m-2 display-4 font-weight-bolder'>
           <b>Next Generation Funded Trading</b>
         </h1>
